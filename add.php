3fc345bce4671f78d50c012fd0c65b2625047f9e
GOKHAN ASKIN <gabyzmr23@gmail.com>
2021-12-11 00:36:21 +0300

    updated

--- add.php
+++ add.php
@@ -18,23 +18,22 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 // sql to create table
-$sql = "CREATE TABLE visitors (
+$sql_table = "CREATE TABLE visitors (
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(30) NOT NULL,
 email VARCHAR(30) NOT NULL,
 phone VARCHAR(30),
 photo VARCHAR(30),
-reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-)";
+reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP)";
     
-if ($conn->query($sql) === TRUE) {
+if ($conn->query($sql_table) === TRUE) {
     echo "Table visitors created successfully";
 } else {
     echo "Error creating table: " . $conn->error;
 }
     
 $conn->close();
-$sql = "INSERT INTO visitors (name, email, phone, photo)
+$sql = "INSERT INTO visitors (name , email, phone, photo) 
 VALUES ('$name', '$email', '$phone', '$pic')";
 
 if ($conn->query($sql) === TRUE) {
