2022638659d63ef9d24b2d1c246db1c1438b6962
GOKHAN ASKIN <gabyzmr23@gmail.com>
2021-12-11 13:55:23 +0300

    updated

--- add.php
+++ add.php
@@ -17,22 +17,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-// sql to create table
-$sql_table = "CREATE TABLE visitors (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-name VARCHAR(30) NOT NULL,
-email VARCHAR(30) NOT NULL,
-phone VARCHAR(30),
-photo VARCHAR(30),
-reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP)";
-    
-if ($conn->query($sql_table) === TRUE) {
-    echo "Table visitors created successfully";
-} else {
-    echo "Error creating table: " . $conn->error;
-}
-    
-$conn->close();
+
 $sql = "INSERT INTO visitors (name , email, phone, photo) 
 VALUES ('$name', '$email', '$phone', '$pic')";
 
